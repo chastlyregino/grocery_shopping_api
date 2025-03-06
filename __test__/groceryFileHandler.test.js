cc583aece5a16061b1b39a435f0e7b5e4dd2b10c
Chastly Regino <chastly_regino@icloud.com>
2025-03-06 15:08:35 -0600

    groceryFileHandler tests clean up

--- __test__/groceryFileHandler.test.js
+++ __test__/groceryFileHandler.test.js
@@ -1,10 +1,8 @@
 const fs = require('node:fs')
 const { writeItems, createFileIfNotExist, readItems, addNewItem, removeSpecificItem, toPurchase } = require('.././groceryFileHandler.js')
 
-//jest.mock('fs')
-const mockwriteItems = jest.fn()
+const mockReadItems = jest.fn()
 
-let tempData, newData
 const data = {
     grocery_list: [
         {
@@ -35,58 +33,48 @@ const newItem = {
     purchased: false
 }
 
-const truthyFile = 'data.json'
+const truthyFile = 'testData.json'
 const falsyFile = 'unexistentFile.json'
 const itemToUpdate = 'banana'
 
-describe('File and Data Manipulation', () => {
+describe('File Manipulation', () => {
     test('Creates the file after checking that it does not exist', () => {
-        console.log('1')
         expect(fs.existsSync(falsyFile)).toBe(false)
         createFileIfNotExist(falsyFile)
         expect(fs.existsSync(falsyFile)).toBe(true)
         fs.unlinkSync(falsyFile)
         expect(fs.existsSync(falsyFile)).toBe(false)
     })
+})
+
+describe('File and Data Manipulation', () => {
+    let newData
+
+    beforeEach(() => {
+        writeItems(data, truthyFile)
+    })
 
     test('Read contents of existing file and returns an object with array of objects', () => {
-        console.log('2')
-        tempData = readItems(truthyFile)//
-        writeItems(data, truthyFile)//
         expect(readItems(truthyFile)).toStrictEqual(data)
-        fs.unlinkSync(truthyFile)//
-        writeItems(tempData, truthyFile)//
     })
 
     test('New item added to file', () => {
-        console.log('3')
-        tempData = readItems(truthyFile)//
-        writeItems(data, truthyFile)//
         addNewItem(newItem, truthyFile)
         newData = readItems(truthyFile)
         const list = newData.grocery_list
         expect(list[list.length - 1]).toStrictEqual(newItem)
-        //fs.unlinkSync(truthyFile)//
-        console.log(tempData)
-        writeItems(tempData, truthyFile)//
     })
 
     test('Item "purchased" value updated to true in file', () => {
-        tempData = readItems(truthyFile)//
-        writeItems(data, truthyFile)//
         toPurchase(itemToUpdate, truthyFile)
         newData = readItems(truthyFile)
         const isItemPurchased = newData.grocery_list.find(({itemName}) => itemName === itemToUpdate )
         expect(isItemPurchased.purchased).toBe(true)
-        writeItems(tempData, truthyFile)//
     })
 
     test('Item removed from file', () => {
-        tempData = readItems(truthyFile)//
-        writeItems(data, truthyFile)//
         removeSpecificItem(itemToUpdate, truthyFile)
         newData = readItems(truthyFile)
         expect(newData.grocery_list.includes(itemToUpdate)).toBe(false)
-        writeItems(tempData, truthyFile)//
     })
 })
